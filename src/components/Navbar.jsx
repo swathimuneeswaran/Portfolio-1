188cee38b851bf4a13d725919f0aa3b39131aa62
swathimuneeswaran <swathirajan255@gmail.com>
2024-05-19 18:59:51 +0530

    comm

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import './Navbar.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-  <header className='header'>
-    <Link to="/" className='logo'>Swathi Muneeswaran</Link>
-    <nav className='nav_list'>
-       <Link to="/" className='nav1' >Home</Link>
-       <Link to="/about" className='nav1'>About</Link>
-       <Link to="/education" className='nav1'>Education</Link> 
-        <Link to="/skills" className='nav1'>Skills</Link>
-        <Link to="/projects" className='nav1'>Projects</Link>
-       <Link to="/contact"className='nav1'>Contact</Link>
-    </nav>
-  </header>
-  )
+    <header className='header'>
+      <Link to="/" className='logo'>Swathi Muneeswaran</Link>
+      <div className='hamburger' onClick={toggleMenu}>
+        ☰
+      </div>
+      <nav className={`nav_list ${isOpen ? 'show' : 'hide'}`}>
+        <Link to="/" className='nav1' onClick={toggleMenu}>Home</Link>
+        <Link to="/about" className='nav1' onClick={toggleMenu}>About</Link>
+        <Link to="/education" className='nav1' onClick={toggleMenu}>Education</Link>
+        <Link to="/skills" className='nav1' onClick={toggleMenu}>Skills</Link>
+        <Link to="/projects" className='nav1' onClick={toggleMenu}>Projects</Link>
+        <Link to="/contact" className='nav1' onClick={toggleMenu}>Contact</Link>
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
